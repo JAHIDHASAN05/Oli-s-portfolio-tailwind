c0eec2acefc8491ffa69c7e8b8d8e8920c0820b3
JAHID HASAN <developerjahid@gmail.com>
2023-11-29 23:49:31 +0600

    set up commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from "../../assets/img/logo.png"
+const Navbar = () => {
+    return (
+        <>
+        <nav className='flex justify-between items-center mx-52 p-8'>
+            <img src={logo} alt="" />
+            <ul className='text-lg font-medium flex gap-x-20'>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Portfolio</a>
+                <a href="">Project</a>
+                <a href="">Blog</a>
+            </ul>
+        </nav>   
+        </>
+    );
+};
+
+export default Navbar;
